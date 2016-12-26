35cd12422c1145a99110fed00fb290b33fc3c733
haoyu <haoyu@HAOYU-PC.mshome.net>
2016-12-26 22:01:41 +0800

    test5

--- src/test5/testGit.java
+++ src/test5/testGit.java
@@ -1,5 +1,9 @@
 package test5;
 
-public class testGit {
+import javax.swing.plaf.synth.SynthSeparatorUI;
 
+public class testGit {
+	public static void main(String[] args) {
+		System.out.println("test5");
+	}
 }
